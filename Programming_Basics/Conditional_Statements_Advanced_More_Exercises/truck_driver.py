7881e6f7333f768eb107772c5c8d810e6113fe90
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-20 06:31:33 +0300

    Update truck_driver.py

--- Programming_Basics/Conditional_Statements_Advanced_More_Exercises/truck_driver.py
+++ Programming_Basics/Conditional_Statements_Advanced_More_Exercises/truck_driver.py
@@ -1,21 +1,30 @@
 season = input()
 km_per_month = float(input())
+
 award = 0
 
 if km_per_month <= 5000:
+    
     if season == "Spring" or season == "Autumn":
         award = 0.75
+        
     elif season == "Summer":
         award = 0.90
+        
     elif season == "Winter":
         award = 1.05
+        
 elif 5000 < km_per_month <= 10000:
+    
     if season == "Spring" or season == "Autumn":
         award = 0.95
+        
     elif season == "Summer":
         award = 1.10
+        
     elif season == "Winter":
         award = 1.25
+        
 elif 10000 < km_per_month <= 20000:
     award = 1.45
 
