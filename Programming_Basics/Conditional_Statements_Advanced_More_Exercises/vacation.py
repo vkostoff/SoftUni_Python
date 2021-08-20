bf28cb68b264f0fa434c3d15b5c20dc3a2ad6429
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-20 06:32:02 +0300

    Update vacation.py

--- Programming_Basics/Conditional_Statements_Advanced_More_Exercises/vacation.py
+++ Programming_Basics/Conditional_Statements_Advanced_More_Exercises/vacation.py
@@ -1,30 +1,39 @@
 budget = float(input())
 season = input()
+
 price = 0
 stay = ""
 place = ""
 
 if budget <= 1000:
     stay = "Camp"
+    
     if season == "Summer":
         place = "Alaska"
         price = budget * 0.65
+        
     elif season == "Winter":
         place = "Morocco"
         price = budget * 0.45
+        
 elif 1000 < budget <= 3000:
     stay = "Hut"
+    
     if season == "Summer":
         place = "Alaska"
         price = budget * 0.80
+        
     elif season == "Winter":
         place = "Morocco"
         price = budget * 0.60
+        
 elif budget > 3000:
     stay = "Hotel"
     price = budget * 0.90
+    
     if season == "Summer":
         place = "Alaska"
+        
     elif season == "Winter":
         place = "Morocco"
 
