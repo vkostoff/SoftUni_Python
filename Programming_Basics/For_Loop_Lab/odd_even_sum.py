b8fe1c793cb04c0fbf66a635da41cf3fa0e93846
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-21 06:49:56 +0300

    Update odd_even_sum.py

--- Programming_Basics/For_Loop_Lab/odd_even_sum.py
+++ Programming_Basics/For_Loop_Lab/odd_even_sum.py
@@ -5,14 +5,17 @@ odd_sum = 0
 
 for i in range(1, n + 1):
     number = int(input())
+    
     if i % 2 == 0:
         even_sum += number
+        
     else:
         odd_sum += number
 
 if odd_sum == even_sum:
     print("Yes")
     print(f"Sum = {even_sum}")
+    
 else:
     difference = odd_sum - even_sum
     print("No")
