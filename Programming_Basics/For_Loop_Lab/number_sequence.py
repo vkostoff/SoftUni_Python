d3503c3039d439ec69e4ccb8182a73dbc06b3734
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-21 06:48:40 +0300

    Update number_sequence.py

--- Programming_Basics/For_Loop_Lab/number_sequence.py
+++ Programming_Basics/For_Loop_Lab/number_sequence.py
@@ -1,11 +1,16 @@
 import sys
+
 n = int(input())
+
 max = -sys.maxsize
 min = sys.maxsize
+
 for number in range (1, n + 1):
     value = int(input())
+    
     if value > max:
         max = value
+        
     if value < min:
         min = value
 
