7e9e3c10eeb9e27f79b04034f476252d6c479df6
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-21 06:47:58 +0300

    Update left_and_right_sum.py

--- Programming_Basics/For_Loop_Lab/left_and_right_sum.py
+++ Programming_Basics/For_Loop_Lab/left_and_right_sum.py
@@ -5,13 +5,16 @@ sum_right = 0
 
 for i in range(2 * n):
     number = int(input())
+    
     if i < n:
         sum_left += number
+        
     else:
         sum_right += number
 
 if sum_left == sum_right:
     print(f"Yes, sum = {sum_left}")
+    
 else:
     difference = sum_left - sum_right
     print(f"No, diff = {abs(difference)}")
