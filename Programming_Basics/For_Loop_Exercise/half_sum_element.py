666b1f7ab48320f44817721521cac5a7298350e5
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-22 16:37:25 +0300

    Add files via upload

---
+++ Programming_Basics/For_Loop_Exercise/half_sum_element.py
@@ -0,0 +1,17 @@
+import sys
+
+numbers = int(input())
+max_number = -sys.maxsize
+number_sum = 0
+for number in range(numbers):
+    current_number = int(input())
+    if current_number > max_number:
+        max_number = current_number
+    number_sum += current_number
+if max_number == number_sum - max_number:
+    print("Yes")
+    print(f"Sum = {max_number}")
+else:
+    difference = abs(max_number - (number_sum - max_number))
+    print("No")
+    print(f"Diff = {difference}")
