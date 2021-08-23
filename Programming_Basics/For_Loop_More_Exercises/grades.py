a7402a3eb0b6cd9f6df2dab2c5bc44e9a479eff3
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-23 06:46:33 +0300

    Update grades.py

--- Programming_Basics/For_Loop_More_Exercises/grades.py
+++ Programming_Basics/For_Loop_More_Exercises/grades.py
@@ -1,20 +1,26 @@
 students = int(input())
+
 grade_two = 0
 grade_three = 0
 grade_four = 0
 grade_five = 0
 total_grade = 0
+
 for students in range(1, students + 1):
     grade = float(input())
+    
     if 2.00 <= grade <= 2.99:
         grade_two += 1
         total_grade += grade
+        
     if 3.00 <= grade <= 3.99:
         grade_three += 1
         total_grade += grade
+        
     if 4.00 <= grade <= 4.99:
         grade_four += 1
         total_grade += grade
+        
     if grade >= 5.00:
         grade_five += 1
         total_grade += grade
