d4d5d9b43e78cd9b179e1eb3cb37a286a5bab709
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-23 06:44:35 +0300

    Update bills.py

--- Programming_Basics/For_Loop_More_Exercises/bills.py
+++ Programming_Basics/For_Loop_More_Exercises/bills.py
@@ -1,14 +1,17 @@
 months = int(input())
+
 water_bill = 20
 internet_bill = 15
 other_bills = 0
 total_electricity_bill = 0
 total_other_bills = 0
+
 for every_month in range(1, months + 1):
     electricity_bill = float(input())
     other_bills = (electricity_bill + 35) * 1.2
     total_other_bills += other_bills
     total_electricity_bill = total_electricity_bill + electricity_bill
+    
 total_water_bill = water_bill * months
 total_internet_bill = internet_bill * months
 average = (total_other_bills + total_water_bill + total_internet_bill + total_electricity_bill) / months
