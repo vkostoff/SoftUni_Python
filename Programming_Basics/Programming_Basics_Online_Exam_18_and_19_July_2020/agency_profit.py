901a4779ea6248abdc3f76329699dd67fcd0b0ed
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-25 07:11:58 +0300

    Update agency_profit.py

--- Programming_Basics/Programming_Basics_Online_Exam_18_and_19_July_2020/agency_profit.py
+++ Programming_Basics/Programming_Basics_Online_Exam_18_and_19_July_2020/agency_profit.py
@@ -3,9 +3,11 @@ adult_tickets_count = int(input())
 kids_tickets_count = int(input())
 net_price_adult_tickets = float(input())
 net_service_fee = float(input())
+
 net_price_kids_tickets = net_price_adult_tickets * 0.30
 total_price_kids_tickets = net_price_kids_tickets + net_service_fee
 total_price_adult_tickets = net_price_adult_tickets + net_service_fee
+
 profit = ((adult_tickets_count * total_price_adult_tickets) + (kids_tickets_count * total_price_kids_tickets)) * 0.2
 
 print(f"The profit of your agency from {name_aviocompany} tickets is {profit:.2f} lv.")
