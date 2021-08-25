0071b3f2d28be864edcfcbb01bea49f5de79f1b6
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-25 07:13:51 +0300

    Update best_player.py

--- Programming_Basics/Programming_Basics_Online_Exam_18_and_19_July_2020/best_player.py
+++ Programming_Basics/Programming_Basics_Online_Exam_18_and_19_July_2020/best_player.py
@@ -1,19 +1,26 @@
 i = 0
 best_score = 0
 best_player = ""
+
 while i != "END":
     name = input()
+    
     if name != "END":
         goals = int(input())
+        
         if goals > best_score:
             best_score = goals
             best_player = name
+            
         if best_score >= 10:
             break
     else:
         break
+        
 print(f"{best_player} is the best player!")
+
 if best_score >= 3:
     print(f"He has scored {best_score} goals and made a hat-trick !!!")
+    
 else:
     print(f"He has scored {best_score} goals.")
