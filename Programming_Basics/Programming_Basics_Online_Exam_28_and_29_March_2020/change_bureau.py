78fc0e2afa5c51599048b91a05b4f00bf047d151
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-26 07:00:16 +0300

    Update change_bureau.py

--- Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/change_bureau.py
+++ Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/change_bureau.py
@@ -6,6 +6,7 @@ bitcoin_price = bitcoin * 1168
 chinese_price_dollar = chinese * 0.15
 dollar = 1.76
 euro = 1.95
+
 leva = (bitcoin_price + (chinese_price_dollar * dollar))
 euro_money = leva / euro
 commission_percentage = (commission / 100) * euro_money
