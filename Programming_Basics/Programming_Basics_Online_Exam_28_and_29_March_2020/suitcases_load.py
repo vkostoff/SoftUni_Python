237bd71c0a43be04333286f037e27edbfc72f5ad
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-26 07:02:42 +0300

    Update suitcases_load.py

--- Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/suitcases_load.py
+++ Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/suitcases_load.py
@@ -1,16 +1,22 @@
 capacity = float(input())
 command = input()
+
 counter = 0
 is_space = True
+
 while command != "End":
     number = float(command)
+    
     if capacity <= number:
         is_space = False
         break
+        
     capacity -= number
     counter += 1
+    
     if counter % 3 == 0:
         capacity -= number * 0.10
+        
         if capacity < 0:
             is_space = False
             counter -= 1
