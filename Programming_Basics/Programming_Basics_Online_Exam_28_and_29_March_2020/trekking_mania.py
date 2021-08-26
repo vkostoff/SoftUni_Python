63b2264e8bca990c897041d472b01527d688e109
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-26 07:03:45 +0300

    Update trekking_mania.py

--- Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/trekking_mania.py
+++ Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/trekking_mania.py
@@ -1,4 +1,5 @@
 groups_count = int(input())
+
 all_people = 0
 musala = 0
 montblan = 0
@@ -9,14 +10,19 @@ everest = 0
 for groups in range(1, groups_count + 1):
     people_count = int(input())
     all_people += people_count
+    
     if people_count <= 5:
         musala += people_count
+        
     elif 6 <= people_count <= 12:
         montblan += people_count
+        
     elif 13 <= people_count <= 25:
         kilimanjaro += people_count
+        
     elif 26 <= people_count <= 40:
         k2 += people_count
+        
     elif people_count >= 41:
         everest += people_count
 
