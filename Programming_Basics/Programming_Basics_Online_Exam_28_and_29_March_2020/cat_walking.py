5a8cae938009f0405aab31dffc898217346fe55e
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-26 06:59:59 +0300

    Update cat_walking.py

--- Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/cat_walking.py
+++ Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/cat_walking.py
@@ -6,5 +6,6 @@ calories_burnt = (minutes_per_day * 5) * daily_walks
 
 if calories_burnt >= (calories_taken / 2):
     print(f"Yes, the walk for your cat is enough. Burned calories per day: {calories_burnt}.")
+    
 else:
     print(f"No, the walk for your cat is not enough. Burned calories per day: {calories_burnt}.")
