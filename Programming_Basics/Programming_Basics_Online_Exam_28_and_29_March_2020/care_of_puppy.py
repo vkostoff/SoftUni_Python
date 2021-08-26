df3b769b83bf499a549e20ca7341f124eb3d2d40
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-26 06:59:46 +0300

    Update care_of_puppy.py

--- Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/care_of_puppy.py
+++ Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/care_of_puppy.py
@@ -1,13 +1,17 @@
 food = int(input()) * 1000
 command = input()
+
 total_food = 0
+
 while command != "Adopted":
     daily_food = int(command)
     total_food += daily_food
     command = input()
+    
 if total_food <= food:
     leftover = food - total_food
     print(f"Food is enough! Leftovers: {leftover} grams.")
+    
 else:
     needed = total_food - food
     print(f"Food is not enough. You need {needed} grams more.")
