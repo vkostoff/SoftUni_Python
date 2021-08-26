1dc53d68ad7c098ef864ae7f0b54cafb7054afea
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-26 07:01:44 +0300

    Update food_for_pets.py

--- Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/food_for_pets.py
+++ Programming_Basics/Programming_Basics_Online_Exam_28_and_29_March_2020/food_for_pets.py
@@ -1,15 +1,19 @@
 days_count = int(input())
 total_food = float(input())
+
 biscuits = 0
 dog_eaten = 0
 cat_eaten = 0
+
 for days in range(1, days_count + 1):
     dog_per_day = int(input())
     cat_per_day = int(input())
     dog_eaten += dog_per_day
     cat_eaten += cat_per_day
+    
     if days % 3 == 0:
         biscuits += (dog_per_day + cat_per_day) * 0.1
+        
 total_eaten = dog_eaten + cat_eaten
 
 print(f"Total eaten biscuits: {round(biscuits)}gr.")
