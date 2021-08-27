b9b6c07e0b4d1c8e5ca2e591c741a02cae24244d
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-27 06:38:33 +0300

    Update max_number.py

--- Programming_Basics/While_Loop_Lab/max_number.py
+++ Programming_Basics/While_Loop_Lab/max_number.py
@@ -1,11 +1,15 @@
 import sys
+
 max = -sys.maxsize
 
 command = input()
+
 while command != "Stop":
     number = int(command)
+    
     if number > max:
         max = number
 
     command = input()
+    
 print(max)
