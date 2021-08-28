52b4fd4475391c56945cce829174f143060c43b0
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-28 08:39:39 +0300

    Update pld _books.py

--- Programming_Basics/While_Loop_Exercise/pld _books.py
+++ Programming_Basics/While_Loop_Exercise/pld _books.py
@@ -1,15 +1,24 @@
 searched_book = input()
+
 books_counter = 0
+
 book_is_found = False
+
 next_book = input()
+
 while next_book != "No More Books":
+    
     if next_book == searched_book:
         book_is_found = True
         break
+        
     books_counter += 1
+    
     next_book = input()
+    
 if book_is_found:
     print(f"You checked {books_counter} books and found it.")
+    
 else:
     print(f"You checked {books_counter} books.")
     print("The book you search is not here!")
