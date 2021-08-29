7b27dcf28da974476089a278c4af2e678b2e3ec2
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-29 22:01:23 +0300

    Update report_system.py

--- Programming_Basics/While_Loop_More_Exercises/report_system.py
+++ Programming_Basics/While_Loop_More_Exercises/report_system.py
@@ -1,34 +1,46 @@
 total_sum = int(input())
+
 counter = 0
 cash_count = 0
 card_count = 0
 cash_money = 0
 card_money = 0
 total_money = 0
+
 command = input()
+
 while command != "End":
     item_price = int(command)
     counter += 1
+    
     if counter % 2 == 0:
+        
         if item_price < 10:
             print("Error in transaction!")
+            
         else:
             card_count += 1
             card_money += item_price
             total_money += item_price
             print("Product sold!")
+            
     else:
+        
         if item_price > 100:
             print("Error in transaction!")
+            
         else:
             cash_count += 1
             cash_money += item_price
             total_money += item_price
             print("Product sold!")
+            
     if total_sum <= total_money:
         print(f"Average CS: {cash_money / cash_count:.2f}")
         print(f"Average CC: {card_money / card_count:.2f}")
         break
+        
     command = input()
+    
 else:
     print("Failed to collect required money for charity.")
