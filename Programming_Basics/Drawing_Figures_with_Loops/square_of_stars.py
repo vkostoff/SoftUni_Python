df6f1ef2fb26e49cef0c098564373b1241561706
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-30 23:22:27 +0300

    Update square_of_stars.py

--- Programming_Basics/Drawing_Figures_with_Loops/square_of_stars.py
+++ Programming_Basics/Drawing_Figures_with_Loops/square_of_stars.py
@@ -1,11 +1,6 @@
 number = int(input())
+
 for i in range(1, number + 1):
     rectangle = number * "* "
+    
     print(rectangle)
-
-# side = int(input())
-# side_b = side
-# for i in range(0, side):
-#     for j in range(0, side_b):
-#         print("* ", end="")
-#     print()
