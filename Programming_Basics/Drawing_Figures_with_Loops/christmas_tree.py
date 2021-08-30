7fe4b8dd6bb0dcea7e482662fc0464f452515263
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-08-30 23:19:59 +0300

    Update christmas_tree.py

--- Programming_Basics/Drawing_Figures_with_Loops/christmas_tree.py
+++ Programming_Basics/Drawing_Figures_with_Loops/christmas_tree.py
@@ -1,8 +1,12 @@
 number = int(input())
+
 free_space = number
 star = 0
+
 print(free_space * " " + star * "*" + " | " + star * "*" + free_space * " ")
+
 for i in range(1, number + 1):
     free_space -= 1
     star += 1
+    
     print(free_space * " " + star * "*" + " | " + star * "*" + free_space * " ")
