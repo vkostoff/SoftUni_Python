181679cb0b15df031fec02e9c0571332a19cb91e
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-01 21:19:40 +0300

    Update godzilla_vs_kong.py

--- Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/godzilla_vs_kong.py
+++ Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/godzilla_vs_kong.py
@@ -1,17 +1,23 @@
 budget = float(input())
 people = int(input())
 person_outfit = float(input())
+
 decor = budget * 0.1
 price_outfit = person_outfit * people
+
 if people > 150:
     price_outfit *= 0.9
+    
 all_money = decor + price_outfit
+
 if budget >= all_money:
     money_left = budget - all_money
+    
     print("Action!")
     print(f"Wingard starts filming with {money_left:.2f} leva left.")
 
 else:
     money_needed = all_money - budget
+    
     print("Not enough money!")
     print(f"Wingard needs {money_needed:.2f} leva more.")
