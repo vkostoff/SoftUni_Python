02a438744ea9413f231beb2b466051030edbb3d2
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-01 21:20:31 +0300

    Update movie_ratings.py

--- Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/movie_ratings.py
+++ Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/movie_ratings.py
@@ -1,22 +1,29 @@
 import sys
 
 movies_count = int(input())
+
 best_movie = ""
 worst_movie = ""
 best_move_rating = 0
 worst_movie_rating = sys.maxsize
 all_rating = 0
+
 for i in range(1, movies_count + 1):
     movie = input()
     rating = float(input())
+    
     all_rating += rating
+    
     if rating > best_move_rating:
         best_movie = movie
         best_move_rating = rating
+        
     elif rating < worst_movie_rating:
         worst_movie = movie
         worst_movie_rating = rating
+        
 average_rating = all_rating / movies_count
+
 print(f"{best_movie} is with highest rating: {best_move_rating:.1f}")
 print(f"{worst_movie} is with lowest rating: {worst_movie_rating:.1f}")
 print(f"Average rating: {average_rating:.1f}")
