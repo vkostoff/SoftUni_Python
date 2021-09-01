76340cd5a3ffb1d1a41f1d07711e8cf5d9bac94b
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-01 21:19:14 +0300

    Update cinema_voucher.py

--- Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/cinema_voucher.py
+++ Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/cinema_voucher.py
@@ -1,19 +1,30 @@
 voucher = int(input())
+
 tickets = 0
 others = 0
+
 command = input()
+
 while command != "End":
     purchase = command
+    
     if len(purchase) > 8:
+        
         if ord(purchase[0]) + ord(purchase[1]) > voucher:
             break
+            
         voucher -= ord(purchase[0]) + ord(purchase[1])
         tickets += 1
+        
     else:
+        
         if ord(purchase[0]) > voucher:
             break
+            
         voucher -= ord(purchase[0])
         others += 1
+       
     command = input()
+    
 print(f"{tickets}")
 print(f"{others}")
