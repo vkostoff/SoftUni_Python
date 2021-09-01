967df16783ab5bb853322016b454a8db37be5027
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-01 21:21:59 +0300

    Update oscars_ceremony.py

--- Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/oscars_ceremony.py
+++ Programming_Basics/Programming_Basics_Online_Exam_6_and_7_April_2019/oscars_ceremony.py
@@ -1,6 +1,8 @@
 rent = int(input())
+
 statues_price = rent * 0.7
 catering = statues_price * 0.85
 sound = catering / 2
 expenses = rent + statues_price + catering + sound
+
 print(f"{expenses:.2f}")
