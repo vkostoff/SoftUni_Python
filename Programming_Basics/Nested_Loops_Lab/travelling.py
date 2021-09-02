4439ff2f1f38d6be01a1d161b602a8907c6c4ec2
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-02 22:59:56 +0300

    Update travelling.py

--- Programming_Basics/Nested_Loops_Lab/travelling.py
+++ Programming_Basics/Nested_Loops_Lab/travelling.py
@@ -1,9 +1,15 @@
 destination = input()
+
 while destination != "End":
     target_money = float(input())
+    
     current_money = 0
+    
     while current_money < target_money:
         saved_money = float(input())
+        
         current_money += saved_money
+        
     print(f"Going to {destination}!")
+    
     destination = input()
