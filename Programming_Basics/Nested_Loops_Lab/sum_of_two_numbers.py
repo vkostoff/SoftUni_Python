1733a474f7ced487076a06133deca14afe2ed410
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-02 22:59:10 +0300

    Add files via upload

---
+++ Programming_Basics/Nested_Loops_Lab/sum_of_two_numbers.py
@@ -0,0 +1,18 @@
+start = int(input())
+end = int(input())
+target_number = int(input())
+counter = 0
+flag = False
+
+for x in range(start, end + 1):
+    for y in range(start, end + 1):
+        combination = x + y
+        counter += 1
+        if combination == target_number:
+            print(f"Combination N:{counter} ({x} + {y} = {target_number})")
+            flag = True
+            break
+    if flag:
+        break
+if not flag:
+    print(f"{counter} combinations - neither equals {target_number}")
