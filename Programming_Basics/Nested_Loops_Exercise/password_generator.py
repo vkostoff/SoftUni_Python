dbb7a07964bebfb624dc36878d08648d8914c08e
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-03 22:02:36 +0300

    Update password_generator.py

--- Programming_Basics/Nested_Loops_Exercise/password_generator.py
+++ Programming_Basics/Nested_Loops_Exercise/password_generator.py
@@ -1,9 +1,15 @@
 first_number = int(input())
 second_number = int(input())
+
 for first in range(1, first_number):
+    
     for second in range(1, first_number):
+        
         for third in range(97, 97 + second_number):
+            
             for fourth in range(97, 97 + second_number):
+                
                 for fifth in range(1, first_number + 1):
+                    
                     if fifth > first and fifth > second:
                         print(f"{first}{second}{chr(third)}{chr(fourth)}{fifth}", end=" ")
