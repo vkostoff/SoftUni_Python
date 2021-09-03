acadc8678c5bea73ac8bc06c9d29b54972fe61c6
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-03 22:01:43 +0300

    Update equal_sums_even_odd_position.py

--- Programming_Basics/Nested_Loops_Exercise/equal_sums_even_odd_position.py
+++ Programming_Basics/Nested_Loops_Exercise/equal_sums_even_odd_position.py
@@ -1,13 +1,19 @@
 first_number = int(input())
 second_number = int(input())
+
 for n in range(first_number, second_number + 1):
     n_to_str = str(n)
+    
     even_sum = 0
     odd_sum = 0
+    
     for index, digit in enumerate(n_to_str):
+        
         if index % 2 == 0:
             even_sum += int(digit)
+            
         else:
             odd_sum += int(digit)
+            
     if even_sum == odd_sum:
         print(n, end=" ")
