475ac133a77c4297ca2d485b87e3be3f7af79afc
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-03 22:03:33 +0300

    Update sum_prime_non_prime.py

--- Programming_Basics/Nested_Loops_Exercise/sum_prime_non_prime.py
+++ Programming_Basics/Nested_Loops_Exercise/sum_prime_non_prime.py
@@ -1,24 +1,36 @@
 prime = 0
 non_prime = 0
+
 is_prime = True
+
 command = input()
+
 while command != "stop":
     number = int(command)
+    
     if number < 0:
         print("Number is negative.")
+        
     elif number == 1:
         non_prime += 1
+        
     elif number > 1:
+        
         for n in range(2, number):
+            
             if (number % n) == 0 and n != number:
                 is_prime = False
                 break
+                
             else:
                 is_prime = True
+                
         if is_prime:
             prime += number
+            
         else:
             non_prime += number
+            
     command = input()
 
 print(f"Sum of all prime numbers is: {prime}")
