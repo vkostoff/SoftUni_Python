14fbdbc13fcd28511851bcc2444039113402e336
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-04 21:43:51 +0300

    Update letters_combinations.py

--- Programming_Basics/Nested_Loops_More_Exercises/letters_combinations.py
+++ Programming_Basics/Nested_Loops_More_Exercises/letters_combinations.py
@@ -1,16 +1,26 @@
 first = input()
 second = input()
 third = input()
+
 counter = 0
+
 for f in range(ord(first), ord(second) + 1):
+    
     if f == ord(third):
         continue
+        
     for t in range(ord(first), ord(second) + 1):
+        
         if t == ord(third):
             continue
+            
         for s in range(ord(first), ord(second) + 1):
+            
             if s == ord(third):
                 continue
+                
             print(f"{chr(f)}{chr(t)}{chr(s)}", end=" ")
+            
             counter += 1
+            
 print(counter)
