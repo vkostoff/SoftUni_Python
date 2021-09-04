dd1984f4631ddf3909cf0c403e11ab9c9f8e6289
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-04 21:48:00 +0300

    Update unique_pin_codes.py

--- Programming_Basics/Nested_Loops_More_Exercises/unique_pin_codes.py
+++ Programming_Basics/Nested_Loops_More_Exercises/unique_pin_codes.py
@@ -1,14 +1,20 @@
 upper_first = int(input())
 upper_second = int(input())
 upper_third = int(input())
+
 for first in range(2, upper_first + 1):
+    
     if first % 2 != 0:
         continue
+        
     for second in range(2, upper_second + 1):
+        
         if second == 4 or second == 6 or second > 7:
             continue
+            
         for third in range(2, upper_third + 1):
             if third % 2 != 0:
                 continue
+                
             print(f"{first} {second} {third}", end="")
             print()
