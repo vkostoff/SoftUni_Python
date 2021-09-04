3e279ca243cbe6846bbc9503bbd4f01f24008a26
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-04 21:44:36 +0300

    Update prime_pairs.py

--- Programming_Basics/Nested_Loops_More_Exercises/prime_pairs.py
+++ Programming_Basics/Nested_Loops_More_Exercises/prime_pairs.py
@@ -2,8 +2,11 @@ first_beginning = int(input())
 second_beginning = int(input())
 first_difference = int(input())
 second_difference = int(input())
+
 for first in range(first_beginning, (first_beginning + first_difference) + 1):
+    
     for second in range(second_beginning, (second_beginning + second_difference) + 1):
+        
         if first % 2 != 0 and second % 2 != 0 and first % 3 != 0 and second % 3 != 0\
                 and first % 5 != 0 and second % 5 != 0 and first % 7 != 0 and second % 7 != 0:
             print(f"{first}{second}")
