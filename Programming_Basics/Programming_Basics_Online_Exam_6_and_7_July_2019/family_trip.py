4f1b3f8742eb24597219f3c292b41bc65453f1d2
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-05 07:31:52 +0300

    Update family_trip.py

--- Programming_Basics/Programming_Basics_Online_Exam_6_and_7_July_2019/family_trip.py
+++ Programming_Basics/Programming_Basics_Online_Exam_6_and_7_July_2019/family_trip.py
@@ -2,13 +2,19 @@ budget = float(input())
 accommodations = int(input())
 price_per_accommodation = float(input())
 additional_expenses_percent = int(input())
+
 if accommodations > 7:
     price_per_accommodation *= 0.95
+    
 additional_expenses = budget * (additional_expenses_percent / 100)
 total_expenses = accommodations * price_per_accommodation + additional_expenses
+
 if budget >= total_expenses:
     money_left = budget - total_expenses
+    
     print(f"Ivanovi will be left with {money_left:.2f} leva after vacation.")
+    
 else:
     money_needed = total_expenses - budget
+    
     print(f"{money_needed:.2f} leva needed.")
