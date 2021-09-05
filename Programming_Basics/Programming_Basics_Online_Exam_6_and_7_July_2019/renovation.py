1f42338f414e6acaa1064f568566d336fe411e8b
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-05 07:33:52 +0300

    Update renovation.py

--- Programming_Basics/Programming_Basics_Online_Exam_6_and_7_July_2019/renovation.py
+++ Programming_Basics/Programming_Basics_Online_Exam_6_and_7_July_2019/renovation.py
@@ -1,26 +1,39 @@
 from math import ceil
+
 wall_height = int(input())
 wall_width = int(input())
 non_painted_percentage = int(input())
+
 to_be_painted = 4 * (wall_height * wall_width)
 not_to_be_painted = to_be_painted * (non_painted_percentage / 100)
 total_to_be_painted = to_be_painted - not_to_be_painted
+
 painted = 0
+
 paint_enough = False
+
 command = input()
+
 while command != "Tired!":
     litters_paint = int(command)
+    
     painted += litters_paint
+    
     if painted >= ceil(total_to_be_painted):
         paint_enough = True
         break
+        
     command = input()
+    
 if paint_enough:
+    
     if painted - total_to_be_painted == 0:
         print("All walls are painted! Great job, Pesho!")
+        
     else:
         paint_left = painted - total_to_be_painted
         print(f"All walls are painted and you have {int(paint_left)} l paint left!")
+        
 else:
     space_left = total_to_be_painted - painted
     print(f"{ceil(space_left)} quadratic m left." )
