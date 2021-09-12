862217bbd901ba7463ce3d804ca7bce331d91b78
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-12 18:10:38 +0300

    Update movie_stars.py

--- Programming_Basics/Programming_Basics_Online_Exam_9_and_10_March_2019/movie_stars.py
+++ Programming_Basics/Programming_Basics_Online_Exam_9_and_10_March_2019/movie_stars.py
@@ -1,18 +1,27 @@
 budget = float(input())
+
 out_of_budget = False
+
 command = input()
+
 while command != "ACTION":
     actor_name = command
+    
     if len(actor_name) > 15:
         budget *= 0.8
+        
     else:
         commission = float(input())
         budget -= commission
+        
     if budget <= 0:
         out_of_budget = True
         break
+        
     command = input()
+    
 if out_of_budget:
     print(f"We need {abs(budget):.2f} leva for our actors.")
+    
 else:
     print(f"We are left with {budget:.2f} leva.")
