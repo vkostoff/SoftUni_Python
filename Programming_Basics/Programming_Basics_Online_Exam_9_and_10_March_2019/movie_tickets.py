3cff647f45382404406b4ae0865dd893b9edd0bc
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-09-12 18:11:00 +0300

    Update movie_tickets.py

--- Programming_Basics/Programming_Basics_Online_Exam_9_and_10_March_2019/movie_tickets.py
+++ Programming_Basics/Programming_Basics_Online_Exam_9_and_10_March_2019/movie_tickets.py
@@ -1,10 +1,15 @@
 a1 = int(input())
 a2 = int(input())
 n = int(input())
+
 c_end = n / 2
+
 for a in range(a1, a2):
+    
     for b in range(1, n):
+        
         for c in range(1, int(c_end)):
             d = a
+            
             if d % 2 != 0 and (b + c + d) % 2 != 0:
                 print(f"{chr(a)}-{b}{c}{d}")
