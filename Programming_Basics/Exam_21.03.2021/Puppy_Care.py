7899d36f0d1b81dc583e3378111327e5dec149d3
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-25 07:23:52 +0300

    Update Puppy_Care.py

--- Programming_Basics/Exam_21.03.2021/Puppy_Care.py
+++ Programming_Basics/Exam_21.03.2021/Puppy_Care.py
@@ -1,12 +1,14 @@
 food_bought = int(input())
 food_bought_grams = food_bought * 1000
+
 command = input()
+
 while command != "Adopted":
     grams_eaten_per_meal = int(command)
     food_bought_grams -= grams_eaten_per_meal
     command = input()
+    
 if food_bought_grams < 0:
     print(f"Food is not enough. You need {abs(food_bought_grams)} grams more." )
 else:
-
     print(f"Food is enough! Leftovers: {food_bought_grams} grams." )
