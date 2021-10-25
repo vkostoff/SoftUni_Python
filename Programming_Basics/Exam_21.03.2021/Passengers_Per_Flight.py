0e788acd2cdd8f52c6866bd9bb2846d3ef1fe272
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-25 07:23:25 +0300

    Update Passengers_Per_Flight.py

--- Programming_Basics/Exam_21.03.2021/Passengers_Per_Flight.py
+++ Programming_Basics/Exam_21.03.2021/Passengers_Per_Flight.py
@@ -1,20 +1,27 @@
 from math import floor
+
 best_aircompany = ""
 best_passengers = 0
+
 aircompanies = int(input())
+
 for aircompanies in range(1, aircompanies + 1):
     aircompany_name = input()
     total_passengers = 0
     counter = 0
     command = input()
+    
     while command != "Finish":
         passengers = int(command)
         total_passengers += passengers
         counter += 1
         command = input()
+        
     average_passengers = total_passengers / counter
     print(f"{aircompany_name}: {floor(average_passengers)} passengers.")
+    
     if average_passengers > best_passengers:
         best_passengers = floor(average_passengers)
         best_aircompany = aircompany_name
+        
 print(f"{best_aircompany} has most passengers per flight: {best_passengers}")
