30261ec46cce332c10ca2bea4cec1f68a7a00365
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-26 06:45:19 +0300

    Update division_without_remainder.py

--- Programming_Basics/Programming_Basics_Online_Retake_Exam_2_and_3_May_2019/division_without_remainder.py
+++ Programming_Basics/Programming_Basics_Online_Retake_Exam_2_and_3_May_2019/division_without_remainder.py
@@ -1,20 +1,25 @@
 numbers_count = int(input())
+
 p1 = 0
 p2 = 0
 p3 = 0
 total = 0
+
 for numbers in range(1, numbers_count + 1):
     number = int(input())
     total += 1
+    
     if number % 2 == 0:
         p1 += 1
     if number % 3 == 0:
         p2 += 1
     if number % 4 == 0:
         p3 += 1
+        
 p1_percentage = p1 / total * 100
 p2_percentage = p2 / total * 100
 p3_percentage = p3 / total * 100
+
 print(f"{p1_percentage:.2f}%")
 print(f"{p2_percentage:.2f}%")
 print(f"{p3_percentage:.2f}%")
