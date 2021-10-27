63e6d568c995685a8e8534ffc36df014ca661657
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-27 06:27:20 +0300

    Update easter_lunch.py

--- Programming_Basics/Programming_Basics_Online_Exam_20_and_21_April_2019/easter_lunch.py
+++ Programming_Basics/Programming_Basics_Online_Exam_20_and_21_April_2019/easter_lunch.py
@@ -1,9 +1,11 @@
 breads_count = int(input())
 egg_cartons = int(input())
 cookies_kg = int(input())
+
 breads_price = breads_count * 3.2
 egg_price = egg_cartons * 4.35
 cookies_price = cookies_kg * 5.4
 paint_price = (egg_cartons * 12) * 0.15
 total = breads_price + egg_price + cookies_price + paint_price
+
 print(f"{total:.2f}")
