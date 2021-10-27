66751cfcd90843391d3336605e8928cf625c9372
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-27 06:26:20 +0300

    Update easter_eggs.py

--- Programming_Basics/Programming_Basics_Online_Exam_20_and_21_April_2019/easter_eggs.py
+++ Programming_Basics/Programming_Basics_Online_Exam_20_and_21_April_2019/easter_eggs.py
@@ -1,29 +1,39 @@
 painted_eggs_count = int(input())
+
 red = 0
 orange = 0
 blue = 0
 green = 0
 max_count = 0
 max_color = ""
+
 for eggs in range(1, painted_eggs_count + 1):
     color = input()
+    
     if color == "red":
         red += 1
+        
         if red > max_count:
             max_count = red
             max_color = "red"
+            
     elif color == "orange":
         orange += 1
+        
         if orange > max_count:
             max_count = orange
             max_color = "orange"
+            
     elif color == "blue":
         blue += 1
+        
         if blue > max_count:
             max_count = blue
             max_color = "blue"
+            
     elif color == "green":
         green += 1
+        
         if green > max_count:
             max_count = green
             max_color = "green"
