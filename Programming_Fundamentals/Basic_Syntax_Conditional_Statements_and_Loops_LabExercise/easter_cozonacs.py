c64a186e7b9df1ac74d1a430e014fa702161886a
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-29 17:52:33 +0300

    Update easter_cozonacs.py

--- Programming_Fundamentals/Basic_Syntax_Conditional_Statements_and_Loops_LabExercise/easter_cozonacs.py
+++ Programming_Fundamentals/Basic_Syntax_Conditional_Statements_and_Loops_LabExercise/easter_cozonacs.py
@@ -1,15 +1,19 @@
 budget = float(input())
 flour_price = float(input())
+
 eggs_price = flour_price * 0.75
 liter_milk_price = flour_price * 1.25
 milk_price = liter_milk_price / 4
 price_per_cozonac = flour_price + eggs_price + milk_price
 colored_eggs = 0
 cozonacs = 0
+
 while budget > price_per_cozonac:
     colored_eggs += 3
     cozonacs += 1
     budget -= price_per_cozonac
+    
     if cozonacs % 3 == 0:
         colored_eggs -= (cozonacs - 2)
+        
 print(f"You made {cozonacs} cozonacs! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
