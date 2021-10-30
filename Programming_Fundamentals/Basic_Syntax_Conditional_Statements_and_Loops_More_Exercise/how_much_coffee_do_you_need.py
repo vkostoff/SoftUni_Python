3d810645b864415d076717beafbb27add3dd0ea9
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-10-30 18:59:50 +0300

    Update how_much_coffee_do_you_need.py

--- Programming_Fundamentals/Basic_Syntax_Conditional_Statements_and_Loops_More_Exercise/how_much_coffee_do_you_need.py
+++ Programming_Fundamentals/Basic_Syntax_Conditional_Statements_and_Loops_More_Exercise/how_much_coffee_do_you_need.py
@@ -1,18 +1,25 @@
 coffee = 0
 need_sleep = False
+
 command = input()
 while command != "END":
+    
     if command == "dog".lower() or command == "cat".lower() or command == "coding".lower()\
             or command == "movie".lower():
         coffee += 1
+        
     elif command == "dog".upper() or command == "cat".upper() or command == "coding".upper()\
             or command == "movie".upper():
         coffee += 2
+        
     if coffee > 5:
         need_sleep = True
         break
+        
     command = input()
+    
 if need_sleep:
     print("You need extra sleep")
+    
 else:
     print(coffee)
