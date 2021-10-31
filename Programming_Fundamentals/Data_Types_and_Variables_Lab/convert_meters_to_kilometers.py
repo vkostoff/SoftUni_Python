0726135bb307047a80b9eb47b04d038cfa9e9926
vkostoff <kostoff91@gmail.com>
2021-10-31 18:12:14 +0200

    Data_Types_and_Variables_Lab

---
+++ Programming_Fundamentals/Data_Types_and_Variables_Lab/convert_meters_to_kilometers.py
@@ -0,0 +1,3 @@
+distance_in_meters = int(input())
+distance_in_kilometers = distance_in_meters * 0.001
+print(f"{distance_in_kilometers:.2f}")
---
+++ Programming_Fundamentals/Data_Types_and_Variables_Lab/special_numbers.py
@@ -0,0 +1,11 @@
+number = int(input())
+for n in range(1, number + 1):
+    digits_sum = 0
+    digits = n
+    while digits > 0:
+        digits_sum += digits % 10
+        digits = int(digits / 10)
+    if (digits_sum == 5) or (digits_sum == 7) or (digits_sum == 11):
+        print(f"{n} -> True")
+    else:
+        print(f"{n} -> False")
