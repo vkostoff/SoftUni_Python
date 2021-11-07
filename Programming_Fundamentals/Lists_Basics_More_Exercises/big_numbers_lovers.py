464f33583ff8cb45826dd4cc7c6629d5e0d1d92f
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-07 06:45:34 +0200

    Update big_numbers_lovers.py

--- Programming_Fundamentals/Lists_Basics_More_Exercises/big_numbers_lovers.py
+++ Programming_Fundamentals/Lists_Basics_More_Exercises/big_numbers_lovers.py
@@ -1,9 +1,13 @@
 numbers = input().split(" ")
 number_list = []
 num = ""
+
 for n in numbers:
     number_list.append(n)
+    
 number_list.sort(reverse=True)
+
 for i in number_list:
     num += i
+    
 print(int(num))
