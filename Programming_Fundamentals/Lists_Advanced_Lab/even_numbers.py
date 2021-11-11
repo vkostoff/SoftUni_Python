d95f21c7d88535eea78c001f41e2b8820a8bbe2a
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-11 06:33:10 +0200

    Update even_numbers.py

--- Programming_Fundamentals/Lists_Advanced_Lab/even_numbers.py
+++ Programming_Fundamentals/Lists_Advanced_Lab/even_numbers.py
@@ -1,6 +1,8 @@
 list_of_numbers = [int(x) for x in input().split(", ")]
 new_list = []
+
 for i in range(len(list_of_numbers)):
     if list_of_numbers[i] % 2 == 0:
         new_list.append(i)
+        
 print(new_list)
