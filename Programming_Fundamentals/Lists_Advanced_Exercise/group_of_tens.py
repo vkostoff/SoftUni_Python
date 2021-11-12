c9ce33e238e663634a213c98cdf59de9926f8db3
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-12 22:46:08 +0200

    Update group_of_tens.py

--- Programming_Fundamentals/Lists_Advanced_Exercise/group_of_tens.py
+++ Programming_Fundamentals/Lists_Advanced_Exercise/group_of_tens.py
@@ -1,15 +1,20 @@
 integers_list = [int(x) for x in input().split(", ")]
 temporary_list = []
+
 boundary = 10
 upper_boundary = 0
 counter = 0
+
 while len(integers_list) > counter:
     for number in integers_list:
+        
         if upper_boundary < number <= boundary:
             temporary_list.append(number)
             counter += 1
             number = -1
+            
     print(f"Group of {boundary}'s: {temporary_list}")
+    
     temporary_list.clear()
     boundary += 10
     upper_boundary += 10
