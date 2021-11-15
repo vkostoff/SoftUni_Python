e73232eb2611a6488b540ff5d72ee39a0c861e32
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-15 23:16:55 +0200

    Update zoo.py

--- Programming_Fundamentals/Objects_and_Classes_Lab/zoo.py
+++ Programming_Fundamentals/Objects_and_Classes_Lab/zoo.py
@@ -10,8 +10,10 @@ class Zoo:
     def add_animal(self, species, name):
         if species == "mammal":
             self.mammals.append(name)
+            
         elif species == "fish":
             self.fishes.append(name)
+            
         elif species == "bird":
             self.birds.append(name)
 
@@ -21,21 +23,27 @@ class Zoo:
         result = ""
         if species == "mammal":
             result += f"Mammals in {self.name}: {', '.join(self.mammals)}\n"
+            
         elif species == "fish":
             result += f"Fishes in {self.name}: {', '.join(self.fishes)}\n"
+            
         elif species == "bird":
             result += f"Birds in {self.name}: {', '.join(self.birds)}\n"
+            
         result += f"Total animals: {Zoo.__animals}"
+        
         return result
 
 
 zoo_name = input()
 zoo = Zoo(zoo_name)
 n = int(input())
+
 for num in range(n):
     token = input().split()
     species = token[0]
     name = token[1]
     zoo.add_animal(species, name)
+    
 info = input()
 print(zoo.get_info(info))
