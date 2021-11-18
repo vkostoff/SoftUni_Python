19f4cd0b0562dd52549b8bc53324cfd49f97371b
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-18 23:44:13 +0200

    Update heart_delivery.py

--- Programming_Fundamentals/04_Programming_Fundamentals_Mid_Exam/heart_delivery.py
+++ Programming_Fundamentals/04_Programming_Fundamentals_Mid_Exam/heart_delivery.py
@@ -1,30 +1,39 @@
 neighborhood = [int(x) for x in input().split("@")]
 current_position = 0
 token = input()
+
 while not token == "Love!":
     token = token.split()
     command = token[0]
     distance = int(token[1])
     current_position += distance
+    
     if current_position >= len(neighborhood):
         current_position = 0
+        
     if neighborhood[current_position] == 0:
         print(f"Place {current_position} already had Valentine's day.")
+        
     else:
         neighborhood[current_position] -= 2
         if neighborhood[current_position] == 0:
+            
             print(f"Place {current_position} has Valentine's day.")
+            
     token = input()
 
 unsuccessful_houses = 0
 successful = True
+
 for house in neighborhood:
     if house > 0:
         unsuccessful_houses += 1
         successful = False
 
 print(f"Cupid's last position was {current_position}.")
+
 if successful:
     print("Mission was successful.")
+    
 else:
     print(f"Cupid has failed {unsuccessful_houses} places.")
