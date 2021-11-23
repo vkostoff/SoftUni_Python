700c68bf30e5a7eba7ee23d7675cf5acf8f9a96c
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-23 06:18:50 +0200

    Update shoot_for_the_win.py

--- Programming_Fundamentals/03_Programming_Fundamentals_Mid_Exam_Retake/shoot_for_the_win.py
+++ Programming_Fundamentals/03_Programming_Fundamentals_Mid_Exam_Retake/shoot_for_the_win.py
@@ -1,17 +1,24 @@
 targets = [int(x) for x in input().split()]
 targets_shot = 0
 token = input()
+
 while not token == "End":
     index = int(token)
+    
     if index < len(targets) and not targets[index] == -1:
         current_integer = targets[index]
         targets[index] = -1
         targets_shot += 1
+        
         for i in range(len(targets)):
             if not targets[i] == -1 and targets[i] > current_integer:
                 targets[i] -= current_integer
+                
             elif not targets[i] == -1 and targets[i] <= current_integer:
                 targets[i] += current_integer
+                
     token = input()
+    
 targets = [str(y) for y in targets]
+
 print(f"Shot targets: {targets_shot} -> {' '.join(targets)}")
