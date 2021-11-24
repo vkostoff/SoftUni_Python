a9141b9d14f77b57aeb8cfc82b17ec70d9c8092c
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-24 22:39:32 +0200

    Update array_modifier.py

--- Programming_Fundamentals/02_Programming_Fundamentals_Mid_Exam/array_modifier.py
+++ Programming_Fundamentals/02_Programming_Fundamentals_Mid_Exam/array_modifier.py
@@ -1,14 +1,20 @@
 array = [int(x) for x in input().split()]
 token = input()
+
 while not token == "end":
     token = token.split()
+    
     if token[0] == "swap":
         array[int(token[1])], array[int(token[2])] = array[int(token[2])], array[int(token[1])]
+        
     elif token[0] == "multiply":
         array[int(token[1])] *= array[int(token[2])]
+        
     elif token[0] == "decrease":
         for i in range(len(array)):
             array[i] -= 1
+            
     token = input()
+    
 array = [str(x) for x in array]
 print(", ".join(array))
