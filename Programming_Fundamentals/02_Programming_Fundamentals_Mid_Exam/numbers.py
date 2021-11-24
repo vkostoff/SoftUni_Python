55aacfad0545f58f8534cd32dabe63017923d25c
vkostoff <kostoff91@gmail.com>
2021-11-24 22:38:24 +0200

    Programming_Fundamentals/02_Programming_Fundamentals_Mid_Exam/

---
+++ Programming_Fundamentals/02_Programming_Fundamentals_Mid_Exam/numbers.py
@@ -0,0 +1,13 @@
+integers = [int(x) for x in input().split()]
+integers.sort(reverse=True)
+average = sum(integers) / len(integers)
+new_list = []
+for i in range(len(integers)):
+    if integers[i] > average:
+        new_list.append(integers[i])
+        if len(new_list) == 5:
+            break
+if len(new_list) > 0:
+    print(*new_list)
+else:
+    print("No")
---
+++ Programming_Fundamentals/02_Programming_Fundamentals_Mid_Exam/softuni_reception.py
@@ -0,0 +1,12 @@
+first_employee = int(input())
+second_employee = int(input())
+third_employee = int(input())
+students_count = int(input())
+hours = 0
+students_per_hour = first_employee + second_employee + third_employee
+while students_count > 0:
+    students_count -= students_per_hour
+    hours += 1
+    if hours % 4 == 0:
+        hours += 1
+print(f"Time needed: {hours}h.")
