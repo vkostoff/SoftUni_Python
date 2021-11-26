86665875a71c5dcc213ae4da62da45abf36cbf25
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-26 22:20:13 +0200

    Update shopping_list.py

--- Programming_Fundamentals/Mid_Exam_Retake_August/shopping_list.py
+++ Programming_Fundamentals/Mid_Exam_Retake_August/shopping_list.py
@@ -3,21 +3,26 @@ groceries = input().split("!")
 command = input()
 while not command == "Go Shopping!":
     command = command.split()
+    
     if command[0] == "Urgent":
         if command[1] not in groceries:
             groceries.insert(0, command[1])
+            
     elif command[0] == "Unnecessary":
         if command[1] in groceries:
             groceries.remove(command[1])
+            
     elif command[0] == "Correct":
         if command[1] in groceries:
             index = groceries.index(command[1])
             groceries.remove(command[1])
             groceries.insert(index, command[2])
+            
     elif command[0] == "Rearrange":
         if command[1] in groceries:
             groceries.remove(command[1])
             groceries.append(command[1])
+            
     command = input()
 
 print(", ".join(groceries))
