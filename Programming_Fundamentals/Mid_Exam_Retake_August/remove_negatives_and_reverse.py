74b59ff2ef64d8a4ab58a1b055bbdcb76b340194
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-26 22:19:49 +0200

    Update remove_negatives_and_reverse.py

--- Programming_Fundamentals/Mid_Exam_Retake_August/remove_negatives_and_reverse.py
+++ Programming_Fundamentals/Mid_Exam_Retake_August/remove_negatives_and_reverse.py
@@ -1,9 +1,12 @@
 numbers = [int(x) for x in input().split()]
 new_list = []
+
 for number in numbers:
     if number >= 0:
         new_list.append(number)
+        
 if len(new_list) > 0:
     print(*list(reversed(new_list)))
+    
 else:
     print("empty")
