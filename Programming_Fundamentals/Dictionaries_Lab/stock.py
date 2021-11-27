c37642ccc2a07363a3a120d5ed137b781b7b6e87
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-11-27 06:52:42 +0200

    Update stock.py

--- Programming_Fundamentals/Dictionaries_Lab/stock.py
+++ Programming_Fundamentals/Dictionaries_Lab/stock.py
@@ -12,5 +12,6 @@ searched_products = input().split()
 for product in searched_products:
     if product in my_dict.keys():
         print(f"We have {my_dict[product]} of {product} left")
+        
     else:
         print(f"Sorry, we don't have {product}")
