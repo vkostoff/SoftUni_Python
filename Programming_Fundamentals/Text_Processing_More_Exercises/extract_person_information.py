393133cd31895ccdf6aa58710eb7d76cde47ed33
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-12-03 21:31:32 +0200

    Update extract_person_information.py

--- Programming_Fundamentals/Text_Processing_More_Exercises/extract_person_information.py
+++ Programming_Fundamentals/Text_Processing_More_Exercises/extract_person_information.py
@@ -4,18 +4,23 @@ for line in range(lines):
     string = input()
     name = ""
     age = ""
+    
     for i in range(len(string)):
         if string[i] == "@":
             for j in range(i+1, len(string)):
                 if not string[j] == "|":
                     name += string[j]
+                    
                 else:
                     break
+                    
     for i in range(len(string)):
         if string[i] == "#":
             for j in range(i+1, len(string)):
                 if not string[j] == "*":
                     age += string[j]
+                    
                 else:
                     break
+                    
     print(f"{name} is {age} years old.")
