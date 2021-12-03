e71ff958cd38b3671b84ce6394c8ed3e3da76cef
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-12-03 21:30:30 +0200

    Update html.py

--- Programming_Fundamentals/Text_Processing_More_Exercises/html.py
+++ Programming_Fundamentals/Text_Processing_More_Exercises/html.py
@@ -1,15 +1,19 @@
 title = input()
 article_content = input()
+
 print("<h1>")
 print(f"    {title}")
 print("</h1>")
 print("<article>")
 print(f"    {article_content}")
 print("</article>")
+
 command = input()
+
 while not command == "end of comments":
     comment = command
     print("<div>")
     print(f"    {comment}")
     print("</div>")
+    
     command = input()
