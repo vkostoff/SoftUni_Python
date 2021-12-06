5f3239dce8ab68f8869435a9bc36d6683fc5e527
Vasil Kostov <78718224+vkostoff@users.noreply.github.com>
2021-12-06 23:34:03 +0200

    Update dragon_army.py

--- Programming_Fundamentals/Dictionaries_More_Exercises/dragon_army.py
+++ Programming_Fundamentals/Dictionaries_More_Exercises/dragon_army.py
@@ -8,8 +8,10 @@ for n in range(number):
 
     if health == "null":
         health = default_health
+        
     if damage == "null":
         damage = default_damage
+        
     if armor == "null":
         armor = default_armor
 
@@ -22,10 +24,13 @@ for k, v in dragons.items():
     avg_damage = 0
     avg_health = 0
     avg_armor = 0
+    
     for key, value in v.items():
         avg_damage += value[0]
         avg_health += value[1]
         avg_armor += value[2]
+        
     print(f"{k}::({avg_damage/len(v):.2f}/{avg_health/len(v):.2f}/{avg_armor/len(v):.2f})")
+    
     for a, b in dragons[k].items():
         print(f"-{a} -> damage: {b[0]}, health: {b[1]}, armor: {b[2]}")
